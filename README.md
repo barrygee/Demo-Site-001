7cd9487a111e3eb59caa998ddd9f9e676b8c758e
Barry <barrymarkgee@gmail.com>
2021-05-03 19:26:26 +0100

    Project commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Demo Site 001 - Recipe site
+
+
+## Technology
+- Static frontend development with HTML, CSS and JavaScript.
+
+- The TailWindCSS framework was used to assist layout.
+
+## Development
+If making changes to the tailwind.config.js or the app > styles.css files, run:
+
+```npm run build```
+
+This will rebuild the project and include the changes made in those files in the public directory.
+
---
+++ public/js/app.js
@@ -0,0 +1,51 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
+
+
+// Modal
+var openmodal = document.querySelectorAll('.modal-open')
+for (var i = 0; i < openmodal.length; i++) {
+    openmodal[i].addEventListener('click', function(event){
+    event.preventDefault()
+    toggleModal()
+    })
+}
+
+const overlay = document.querySelector('.modal-overlay')
+overlay.addEventListener('click', toggleModal)
+
+var closemodal = document.querySelectorAll('.modal-close')
+for (var i = 0; i < closemodal.length; i++) {
+    closemodal[i].addEventListener('click', toggleModal)
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event
+    var isEscape = false
+    if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+    } else {
+    isEscape = (evt.keyCode === 27)
+    }
+    if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal()
+    }
+};
+
+
+function toggleModal () {
+    const body = document.querySelector('body')
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+    body.classList.toggle('modal-active')
+}
+        
